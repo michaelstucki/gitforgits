44955029b8449c88a48badb200da26535d6dc3a9
michaelstucki <stcuthbertus@protonmail.com>
2025-12-05 11:04:34 -0700

    define domain class, Book

---
+++ src/main/java/com/michaelstucki/gitforgits/model/Book.java
@@ -0,0 +1,45 @@
+package com.michaelstucki.gitforgits.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    private Long id;
+    private String title;
+    private String author;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
