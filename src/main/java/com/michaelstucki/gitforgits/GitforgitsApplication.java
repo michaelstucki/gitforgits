c414b45f01818c668356b13679cc8b568a125b57
michaelstucki <stcuthbertus@protonmail.com>
2025-12-05 10:51:09 -0700

    first commit

---
+++ src/main/java/com/michaelstucki/gitforgits/GitforgitsApplication.java
@@ -0,0 +1,13 @@
+package com.michaelstucki.gitforgits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitforgitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitforgitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michaelstucki/gitforgits/GitforgitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelstucki.gitforgits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitforgitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
